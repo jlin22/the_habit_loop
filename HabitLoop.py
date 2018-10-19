16c456d1c90277af24da7312552054f75d2c24e8
John Lin <jlin1674@gmail.com>
2018-10-18 21:04:47 -0400

    created a basic HabitLoop class

---
+++ HabitLoop.py
@@ -0,0 +1,10 @@
+class HabitLoop:
+    
+    def set_cue(self, cue):
+        self.cue = cue
+    def set_routine(self, routine):
+        self.routine = routine
+    def set_reward(self, reward):
+        self.reward = reward
+    def set_date(self, date)
+        self.date = date
