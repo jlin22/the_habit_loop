8ee26f5df9596945710a00540348f509d2e9efd3
John Lin <jlin1674@gmail.com>
2018-10-22 18:13:20 -0400

    finished Habit.py str

--- Habit.py
+++ Habit.py
@@ -1,8 +1,14 @@
+from HabitLoop import HabitLoop
+
 class Habit:
     habit_loops = [] 
     
     def add_habit_loop(self, habit_loop):
         self.habit_loops.append(habit_loop)
-    def 
-        
-    
+    def __str__(self):
+        s = ''
+        for loop in self.habit_loops:
+            s += loop.__str__()
+            s += '\n'
+        return s
+            
