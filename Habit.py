76c7686f6b0dd3916aafba6a19f2f81132c94e9b
John Lin <jlin1674@gmail.com>
2018-10-22 18:33:53 -0400

    making good progress

--- Habit.py
+++ Habit.py
@@ -5,6 +5,8 @@ class Habit:
     
     def add_habit_loop(self, habit_loop):
         self.habit_loops.append(habit_loop)
+    def length(self):
+        return len(self.habit_loops)
     def __str__(self):
         s = ''
         for loop in self.habit_loops:
--- HabitTracker.py
+++ HabitTracker.py
@@ -2,31 +2,62 @@ from Habit import Habit
 from HabitLoop import HabitLoop
 
 class HabitTracker:
-    habits = []
+    habits = {}
     
-    def add_habit(self, habit):
-        self.habits.append(habit) 
-    def __str__(self):
-        return '0'
+    def add_new_category(self, category, habit):
+        self.habits[category] = habit
+    def add_habit_to_existing_category(self, category, loop):
+        self.habits[category].add_habit_loop(loop)
+    def is_empty(self):
+        return len(self.habits) == 0
+    def categories(self):
+        return self.habits.keys()
+    def get_category(self, category):
+        return self.habits[category].__str__()
+    def get_habit_number(self, category):
+        if category not in self.habits.keys():
+            return 1
+        else:
+            return self.habits[category].length()
     
 if __name__ == '__main__':
     all_habits = HabitTracker()
-    #x = input('What would you like to do?\n')
-    x = 'add habit to a new category'
-    if x == 'add habit to a new category':
-        cue = input('What is the cue?\n')
-        routine = input('What is the routine?\n')
-        reward = input('What is the reward?\n') 
-        loop = HabitLoop(cue, routine, reward, 1)        
-        habit = Habit()
-        habit.add_habit_loop(loop) 
-        all_habits.add_habit(habit)
-        print(all_habits)
-    elif x == 'add habit to an old category':
-        pass
-    elif x == 'view habit':
-        pass
-    elif x == 'view all habits':
-        pass
-    else:
-        pass
+    while(True):
+        x = input('What would you like to do?\n')
+        if x == 'add habit to a new category':
+            category = input('What is the category?\n')
+            cue = input('What is the cue?\n')
+            routine = input('What is the routine?\n')
+            reward = input('What is the reward?\n') 
+            loop = HabitLoop(cue, routine, reward, all_habits.get_habit_number(category))        
+            habit = Habit()
+            habit.add_habit_loop(loop)
+            all_habits.add_new_category(category, habit)
+        elif x == 'add habit to an old category':
+            if all_habits.is_empty():
+                print('No old categories')
+                continue
+            category = input('Which category?\n')
+            if category not in all_habits.categories():
+                print('This category does not exist')
+                continue
+            cue = input('What is the cue?\n')
+            routine = input('What is the routine?\n')
+            reward = input('What is the reward?\n') 
+            loop = HabitLoop(cue, routine, reward, all_habits.get_habit_number(category))        
+            all_habits.add_habit_to_existing_category(category, loop)
+        elif x == 'view category':
+            if all_habits.is_empty():
+                print('No category to view')
+                continue
+            category = input('Which category?\n') 
+            if category not in all_habits.categories():
+                print('This category does not exist')
+                continue
+            print(all_habits.get_category(category))
+        elif x == 'view all habits':
+            pass
+        elif x == 'exit':
+            break
+        else:
+            pass
